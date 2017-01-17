83737222dd92d219bbcbf01f30c8f0832fcab711
grxgs <me@grxgs.com>
2017-01-16 19:16:14 -0800

    First commit. More to come soon!

---
+++ chp2/2-1-simple-message.py
@@ -0,0 +1,4 @@
+# Excercise 2-1
+# Simple Message: Store a message in a variable, and then print that message.
+message = "Fancy message inside this variable"
+print(message)
---
+++ chp2/2-2-simple-messages.py
@@ -0,0 +1,9 @@
+# Excercise 2-2
+# Simple Messages: Store a message in a variable, and print that message.
+# Then change the value of your variable to a new message, and print that new
+# message
+
+message = "Another fancy message to print"
+print(message)
+message = "This time there's going to be a different message"
+print(message)
