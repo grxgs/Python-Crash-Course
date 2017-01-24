dece96ca3f0286df92d61554df48f98f4f27c49e
grxgs <me@grxgs.com>
2017-01-23 22:28:52 -0800

    Completed first couple of exercises for Chp 5.

---
+++ chp5/5-1-conditional-tests.py
@@ -0,0 +1,41 @@
+# Exercise 5-1
+
+phone = 'samsung'
+print("Is phone == 'samsung'? I predict True.")
+print(phone == 'samsung')
+
+print("\nIs phone == 'apple'? I predict False.")
+print(phone == 'apple')
+
+phone = 'lg'
+print("\nIs phone == 'apple'? I predict False.")
+print(phone == 'apple')
+
+phone = 'motorola'
+print("\nIs phone == 'apple'? I predict False.")
+print(phone == 'apple')
+
+phone = 'google'
+print("\nIs phone == 'apple'? I predict False.")
+print(phone == 'apple')
+
+phone = 'sony'
+print("\nIs phone == 'apple'? I predict False.")
+print(phone == 'apple')
+
+phone = 'apple'
+print("\nIs phone == 'apple'? I predict True.")
+print(phone == 'apple')
+
+phone = 'lg'
+print("\nIs phone == 'lg'? I predict True.")
+print(phone == 'lg')
+
+phone = 'google'
+print("\nIs phone == 'google'? I predict True.")
+print(phone == 'google')
+
+phone = 'sony'
+print("\nIs phone == 'sony'? I predict True.")
+print(phone == 'sony')
+
---
+++ chp5/5-2-more-conditional-tests.py
@@ -0,0 +1,40 @@
+# Exercise 5-2
+
+print("Testing with strings\n")
+food = "pizza"
+print("Pizza is not equal to pizza")
+print(food == "Pizza")
+
+print("pizza is equal to pizza")
+print(food == "pizza")
+
+print("But Pizza.lower() is equal to pizza")
+print(food == "Pizza".lower())
+
+print("5 is greater than 1")
+print(5 > 1)
+
+print("1 is less than 5")
+print(1 < 5)
+
+print("(2+3) is equal to 5")
+print((2+3) == 5)
+
+print("10 is less than or equal to 20")
+print(10 <= 20)
+
+print("20 is greater than or equal to 10")
+print(20 >= 10)
+
+print("3 is greater than 1 and 2")
+print(3 > 1 and 3 > 2)
+
+print("5 is greater than 6 or 10 is greater than 20")
+print(5 > 6 or 10 > 20)
+
+food = ['pizza', 'sushi', 'steak']
+print("pizza is in the food list")
+print('pizza' in food)
+
+print("chicken is not in the food list")
+print("chicken" not in food)
